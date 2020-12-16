6ee91cd9a5be1a971a1951462f15165d76e83af2
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2020-12-16 16:00:00 -0600

    EOD push (SideBar added and will close on clicking for mobile)

--- myScript.js
+++ myScript.js
@@ -12,4 +12,8 @@ function openNav() {
   }
 
 
-  
+  function closeNavMedia() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
